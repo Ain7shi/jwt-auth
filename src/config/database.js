064d11cca24f79c23b1eb08a07f7246548019306
Ain7shi <franzgabrielleataguan@gmail.com>
2025-09-21 22:21:50 +0800

    Add files via upload

---
+++ src/config/database.js
@@ -0,0 +1,41 @@
+// Sequelize Databse Configuration
+//---------------------------------
+
+
+require('dotenv').config();
+
+module.exports = {
+    development:{
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect:'postgres',
+        Logging: console.log, // Shows SQL queries in console (helpful for debugging)
+    },
+    test:{
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME +'_test',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect:'postgres',
+        Logging: false,
+    },
+    production:{
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database:process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect:'postgres',
+        Logging: false,
+        dialectoptions: {
+            ssl: {
+            require: true,
+            rejectUnauthorized: false
+            }
+        }
+    }
+};
